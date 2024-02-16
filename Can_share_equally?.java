d0f6a8861502042754b10e27a8a28e5eabe6e115
22P31A05B0 <160036855+22P31A05B0@users.noreply.github.com>
2024-02-16 12:01:11 +0530

    import java.util.Scanner; public class equally {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         if(x%2==0 && x!=0||y%2==0 && y!=0)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class equally
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x%2==0 && x!=0||y%2==0 && y!=0)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
