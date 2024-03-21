777c98b241a491ce241b2e496923f84ef2cd1c7f
22P31A05B0 <160036855+22P31A05B0@users.noreply.github.com>
2024-03-21 10:50:05 +0530

    import java.util.Scanner; public class Celsius {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int c=sc.nextInt();         double f=(c*1.8)+32;         System.out.printf("%.2f",f);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Celsius
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        double f=(c*1.8)+32;
+        System.out.printf("%.2f",f);
+    }
+}
